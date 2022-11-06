1ac9b95c9b0fca4030509b7a2598065e8d98751a
Kang-Jh <iamkangjh@gmail.com>
2022-11-06 21:36:52 +0900

    Add comments

--- react-example/src/App.tsx
+++ react-example/src/App.tsx
@@ -7,26 +7,8 @@ function App() {
   const [value, setValue] = useState('');
   const [query, setQuery] = useState('');
   const [errorCount, setErrorCount] = useState(0);
-  const { data } = useSWR(
-    `/auto-complete?q=${query}`,
-    async (key) => {
-      const { data } = await axios.get(key, {
-        timeout: 2000,
-      });
 
-      return data;
-    },
-    {
-      onSuccess() {
-        setErrorCount(0);
-      },
-      onError() {
-        setErrorCount((state) => state + 1);
-      },
-      errorRetryCount: 3,
-    }
-  );
-
+  // 너무 잦은 요청은 부하가 심하니 타이핑 간격이 좁으면(< 500ms) 대기하다가 입력이 늦어지면 그때 서버에 요청하세요.
   useEffect(() => {
     const tid = setTimeout(() => {
       setQuery(value);
@@ -36,6 +18,38 @@ function App() {
       clearTimeout(tid);
     };
   }, [value]);
+
+  // 데이터는 캐시로 보관을 해서 먼저 보여주고 요청이 완료되면 새로 갱신된 데이터를 보여주세요.
+  const { data } = useSWR(
+    `/auto-complete?q=${query}`,
+    async (key) => {
+      const { data } = await axios.get(key, {
+        // 제한 시간
+        timeout: 2000,
+      });
+
+      return data;
+    },
+    {
+      onSuccess() {
+        setErrorCount(0);
+      },
+
+      // 일정 시간(>= 2000ms) 동안 응답이 없으면 3회 재시도 하고
+      errorRetryCount: 3,
+      // 그래도 응답이 없으면 에러 메시지를 출력해 주세요
+      onError() {
+        setErrorCount((state) => state + 1);
+      },
+    }
+  );
+
+  // 엔터를 누르면 서버로 요청한 건 취소하고 검색 결과를 보여주세요.
+  const onEnter = (e: any) => {
+    if (e.key === 'Enter') {
+      setQuery(value);
+    }
+  };
 
   return (
     <div className="App">
@@ -51,11 +65,7 @@ function App() {
         type="text"
         value={value}
         onChange={(e) => setValue(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') {
-            setQuery(value);
-          }
-        }}
+        onKeyDown={onEnter}
       />
     </div>
   );
--- rxjs-example/src/main.ts
+++ rxjs-example/src/main.ts
@@ -35,8 +35,11 @@ let autoCompleteAbortController = new AbortController();
 
 inputValue$
   .pipe(
+    // 너무 잦은 요청은 부하가 심하니 타이핑 간격이 좁으면(< 500ms) 대기하다가 입력이 늦어지면 그때 서버에 요청하세요.
     debounceTime(500),
+    // 같은 내용일때는 요청하지 마세요.
     distinctUntilChanged(),
+    // 기존 요청을 취소하기 위한 사이드 이펙트
     tap(() => {
       autoCompleteAbortController.abort();
       autoCompleteAbortController = new AbortController();
@@ -47,14 +50,24 @@ inputValue$
       }).pipe(
         mergeMap((response) => from(response.json())),
         tap((value) => (cache = { ...cache, [text]: value })),
+        // 일정 시간(>= 2000ms) 동안 응답이 없으면
         timeout(2000),
+        //  3회 재시도 하고
         retry(3),
-        catchError(() => of([]))
+        //  그래도 응답이 없으면 에러 메시지를 출력해 주세요
+        catchError((err) => {
+          if (err.name !== 'AbortError') {
+            alert('Time out');
+          }
+
+          return of([]);
+        })
       )
     )
   )
-  .subscribe((fetchResult) => console.log(`fetch result: `, fetchResult));
+  .subscribe((value) => console.log('fetch result: ', value));
 
+// 데이터는 캐시로 보관을 해서 먼저 보여주고 요청이 완료되면 새로 갱신된 데이터를 보여주세요.
 inputValue$.subscribe((value) => console.log('cache result: ', cache[value]));
 
 enterKeydownEvent$.subscribe(() => {
